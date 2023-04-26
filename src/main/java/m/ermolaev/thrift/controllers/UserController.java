fd84be0ea2c7e16119e7030dcfe6f0215243f8f5
Mikhail <ermol24@icloud.com>
2023-04-26 13:32:25 +0200

    created login and register pages

--- src/main/java/m/ermolaev/thrift/controllers/UserController.java
+++ src/main/java/m/ermolaev/thrift/controllers/UserController.java
@@ -1,5 +1,6 @@
 package m.ermolaev.thrift.controllers;
 
+import jakarta.servlet.http.HttpSession;
 import m.ermolaev.thrift.domain.User;
 import m.ermolaev.thrift.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,62 @@ public class UserController {
 //            // code to display the page
 //        }
 //    }
+
+    @GetMapping("/login")
+    public ModelAndView showLoginPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login/login");
+        return modelAndView;
+    }
+
+    @PostMapping("/login")
+    public ModelAndView login(@RequestParam String username, @RequestParam String password, HttpSession session) {
+        // Authenticate the user here
+        User user = userRepository.findByUsernameAndPassword(username, password);
+        if(user != null){
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.addObject("username", username);
+            modelAndView.setViewName("redirect:/{username}/wallets");
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.addObject("error", "Invalid username or password");
+            modelAndView.setViewName("login");
+            return modelAndView;
+        }
+    }
+
+    @GetMapping("/register")
+    public ModelAndView showRegisterPage() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("login/register");
+        return modelAndView;
+    }
+
+    @PostMapping("/register")
+    public ModelAndView register(@RequestParam String username, @RequestParam String password) {
+        ModelAndView modelAndView = new ModelAndView();
+        try {
+            userRepository.registerUser(username, password);
+            modelAndView.addObject("success", "User registered successfully");
+            modelAndView.addObject("username", username);
+            modelAndView.setViewName("redirect:/{username}/wallets");
+        } catch (Exception e) {
+            modelAndView.addObject("error", e.getMessage());
+            modelAndView.setViewName("redirect:/register");
+        }
+        return modelAndView;
+    }
+
+//    @PostMapping("/login")
+//    public ModelAndView login(@RequestParam String username, @RequestParam String password) {
+//        // Authenticate the user here
+//        ModelAndView modelAndView = new ModelAndView();
+//        modelAndView.addObject("username", username);
+//        modelAndView.setViewName("redirect:/{username}/wallets");
+//        return modelAndView;
+//    }
+
 
     @GetMapping("/{nickname}/profile")
     public ModelAndView profilePage(@PathVariable String nickname) {
--- src/main/java/m/ermolaev/thrift/repositories/UserRepository.java
+++ src/main/java/m/ermolaev/thrift/repositories/UserRepository.java
@@ -2,9 +2,12 @@ package m.ermolaev.thrift.repositories;
 
 import m.ermolaev.thrift.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Repository;
+
 
 import java.util.List;
 
@@ -14,8 +17,40 @@ public class UserRepository {
     JdbcTemplate jdbcTemplate;
 
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT nickname FROM users",
+        return jdbcTemplate.query("SELECT username FROM users",
                 BeanPropertyRowMapper.newInstance(User.class));
     }
+
+    public User findByUsernameAndPassword(String username, String password) {
+        try {
+//            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//            String hashedPassword = passwordEncoder.encode(password);
+            String hashedPassword = password;
+            System.out.println("password: " + password);
+            System.out.println("hashedpassword: " + hashedPassword);
+            User user = jdbcTemplate.queryForObject("SELECT * FROM users WHERE username = ? AND password = ? LIMIT 1",
+                    new Object[]{username, hashedPassword},
+                    BeanPropertyRowMapper.newInstance(User.class));
+            return user;
+        } catch (EmptyResultDataAccessException ex) {
+            return null; // handle the case when no user was found
+        }
+    }
+
+    public void registerUser(String username, String password) throws Exception {
+//        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+//        String hashedPassword = passwordEncoder.encode(password);
+        String hashedPassword = password;
+        String sql = "SELECT COUNT(*) FROM users WHERE username = ?";
+        int count = jdbcTemplate.queryForObject(sql, Integer.class, username);
+        if (count > 0) {
+            throw new Exception("User with username " + username + " already exists.");
+        }
+
+        sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        jdbcTemplate.update(sql, username, hashedPassword);
+    }
+
+
 
 }
