bc5c04dfaae2904d77223869d8f6d3fe648efe55
Mikhail <ermol24@icloud.com>
2023-06-14 15:28:48 +0200

    message stored in DB by rabbitMQ

--- src/main/java/m/ermolaev/thrift/rabbitmq/Notification.java
+++ src/main/java/m/ermolaev/thrift/rabbitmq/Notification.java
@@ -1,12 +1,29 @@
 package m.ermolaev.thrift.rabbitmq;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Notification {
     @JsonProperty("recipient")
-    private String recipient;
+    private Integer recipient;
 
     @JsonProperty("message")
     private String message;
-    // ...
+
+    public Integer getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(Integer recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
--- src/main/java/m/ermolaev/thrift/rabbitmq/RabbitMqListener.java
+++ src/main/java/m/ermolaev/thrift/rabbitmq/RabbitMqListener.java
@@ -1,11 +1,13 @@
 package m.ermolaev.thrift.rabbitmq;
 
 
+import m.ermolaev.thrift.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,10 +15,12 @@ import org.springframework.stereotype.Component;
 public class RabbitMqListener {
     private Logger logger = LoggerFactory.getLogger(RabbitMqListener.class);
 
+    @Autowired
+    private NotificationService notificationService;
+
     @RabbitListener(queues = "myQueue")
     public void processNotification(Notification notification) {
         logger.info("Received notification: {}", notification);
-        // Process the received notification
-        // Implement the logic to send the notification to the appropriate recipient(s), e.g., via email, push notification, etc.
+        notificationService.sendNotification(notification);
     }
 }
