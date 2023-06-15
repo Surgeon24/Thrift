2d4a9fcdf3f4e715d19b9e2ffb549aa544af3934
Mikhail <ermol24@icloud.com>
2023-06-15 05:33:00 +0200

    added basic security and hash passwords

--- src/main/java/m/ermolaev/thrift/ThriftApplication.java
+++ src/main/java/m/ermolaev/thrift/ThriftApplication.java
@@ -1,7 +1,10 @@
 package m.ermolaev.thrift;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationServiceException;
 
 @SpringBootApplication
 public class ThriftApplication {
---
+++ src/main/java/m/ermolaev/thrift/UserDetailsServiceImpl.java
@@ -0,0 +1,41 @@
+package m.ermolaev.thrift;
+
+import m.ermolaev.thrift.domain.User;
+import m.ermolaev.thrift.repositories.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import java.util.Collections;
+import java.util.List;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        // Create a UserDetails object with a single role
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        List<GrantedAuthority> authorities = Collections.singletonList(authority);
+
+        return new org.springframework.security.core.userdetails.User(
+                user.getUsername(),
+                user.getPassword(),
+                authorities
+        );
+    }
+
+}
